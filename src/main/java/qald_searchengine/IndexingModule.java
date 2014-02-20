f160d93146b269d5b2e5aba636f9fcd390c48d4e
Axel Ngonga <ngonga@Axels-MacBook-Air.local>
2014-02-20 07:54:32 +0100

    Works

--- src/main/java/qald_searchengine/IndexingModule.java
+++ src/main/java/qald_searchengine/IndexingModule.java
@@ -9,13 +9,20 @@ import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
@@ -23,47 +30,59 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.node.Node;
+import org.elasticsearch.search.SearchHit;
 
 public class IndexingModule {
 
     String clientIp = "143.248.135.212";
     int port = 9300;
-    String indexName = "okbqa";
+    String documentType = "label";
+    String shardName = "okbqasearch";
+    String indexName = "dbpediasurfaceform";
     Set<String> whiteList = null;
-
+    public static int bulkSize = 1000;
+    public static final Logger log = Logger.getLogger("OPENQA");
+    public int numberOfSearchResults = 10;
+    public boolean verbose = true;
     public static void main(String[] args) {
+        //testIndex();
+        testSearch();
         // TODO Auto-generated method stub
         // on startup
-        try {
+        if (args.length > 0) {
+            try {
 
-            IndexingModule im = new IndexingModule();
-            //im.insertToIndexByFile("C:/workspace/qald_searchengine/dictionaryList/enTest.txt");
+                IndexingModule im = new IndexingModule();
+                //im.insertToIndexByFile("C:/workspace/qald_searchengine/dictionaryList/enTest.txt");
 
-            if (args.length > 1) {
-                im.indexName = args[1];
-            }
-            if (args.length > 2) {
-                im.clientIp = args[2];
-                im.port = Integer.parseInt(args[3]);
-                im.indexName = args[4];
-            }
-            if (args.length > 5) {
-                im.whiteList = getWhiteList(args[5]);
-            }
-            im.insertToIndexByFile(args[0]);
+                if (args.length > 1) {
+                    im.shardName = args[1];
+                }
+                if (args.length > 2) {
+                    im.clientIp = args[2];
+                    im.port = Integer.parseInt(args[3]);
+                    im.shardName = args[4];
+                }
+                if (args.length > 5) {
+                    im.whiteList = getWhiteList(args[5]);
+                }
+                im.insertToIndexByFile(args[0]);
 
-        } catch (ElasticsearchException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            } catch (ElasticsearchException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
 
-    /** Reads the white list from a file (one URI per line)
-     * 
+    /**
+     * Reads the white list from a file (one URI per line)
+     *
      * @param fileName File containing the URIs
      * @return List of URIs whose surface forms are to be indexed
      * @throws IOException If file cannot be read
@@ -78,8 +97,10 @@ public class IndexingModule {
         return whiteList;
     }
 
-    /** Reads a TSV file of surface forms and write it in the index
-     * 
+    /**
+     * Reads a TSV file of surface forms and write it in the index. Loads in
+     * bulk.
+     *
      * @param fileName TSV File
      * @throws ElasticsearchException If server does not work
      * @throws IOException If file cannot be read
@@ -88,8 +109,10 @@ public class IndexingModule {
         Client client = getClient();
         BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF-8"));
         String uri, surfaceForm, str;
-        int index = 0;
+        int index = 1;
         boolean indexing;
+        BulkRequestBuilder bulkRequest = client.prepareBulk();
+        BulkResponse response;
         while ((str = in.readLine()) != null) {
             String[] strArr = str.split("\t");
             uri = strArr[0];
@@ -108,11 +131,22 @@ public class IndexingModule {
                     json.put("type", "Entity");
 
                     //index plays the role of the ID value
-                    IndexResponse response = client.prepareIndex(indexName, "label", "" + index)
-                            .setSource(json)
-                            .execute()
-                            .actionGet();
+                    bulkRequest.add(client.prepareIndex(shardName, documentType, "" + index)
+                            .setSource(json));
+                    if(verbose) log.log(Level.INFO, "Inserting {0}", json);
                     index++;
+                    if (index % bulkSize == 0) {
+                        response = bulkRequest.execute().actionGet();
+                        if (response.hasFailures()) {
+                            log.warning(response.buildFailureMessage());
+                        }
+                    }
+                }
+            }
+            if (bulkRequest.numberOfActions() > 0) {
+                response = bulkRequest.execute().actionGet();
+                if (response.hasFailures()) {
+                    log.warning(response.buildFailureMessage());
                 }
             }
         }
@@ -125,11 +159,44 @@ public class IndexingModule {
      * @return Client for indexing
      */
     public Client getClient() {
-        Settings settings = ImmutableSettings.settingsBuilder().put("cluster.name", indexName).build();
+        Settings settings = ImmutableSettings.settingsBuilder().put("cluster.name", shardName).build();
         TransportClient transportClient = new TransportClient(settings);
         transportClient = transportClient.addTransportAddress(new InetSocketTransportAddress(clientIp, port));
         return (Client) transportClient;
     }
-    
-    
+
+    public Map<String, Float> searchIndex(String searchString) {
+        Map<String, Float> result;
+        result = new HashMap<String, Float>();
+        Client client = getClient();
+        SearchResponse response = client.prepareSearch(indexName)
+                .setSearchType(SearchType.QUERY_AND_FETCH)
+                .setQuery(QueryBuilders.fuzzyQuery("surfaceForm", searchString))
+                .execute()
+                .actionGet();
+        log.info(response.toString());
+        for (SearchHit sh : response.getHits()) {            
+            result.put(sh.sourceAsMap().get("uri").toString(), sh.getScore());            
+        }
+        return result;
+    }
+
+    public static void testSearch() {
+        try {
+            IndexingModule im = new IndexingModule();
+            System.out.println(im.searchIndex("bonapd"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void testIndex() {
+        try {
+            IndexingModule im = new IndexingModule();
+            im.insertToIndexByFile("/Users/ngonga/Downloads/qald_searchengine/dictionaryList/enTest.txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
